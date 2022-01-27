7d99a2082746cc947c1f29931cd0b99652519096
gpt-anurag <anurag.dev1@protonmail.com>
2022-01-27 22:49:50 +0530

    automated testing

--- functions.js
+++ functions.js
@@ -1,10 +1,13 @@
 "use script";
+//pat: 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
 // --> 2.15 Functions
 
 // function message() {
 //   console.log("Message from function");
 // }
 // message();
+
+// Values passed to a function as parameters are copied to its local variables.
 
 // --> A variable declared inside a function is only visible inside the function.
 // --> A variable declared outside a function is outer function. A function can access outer variable and modify it.
@@ -38,3 +41,81 @@
 // function doNothing() {}
 
 // console.log(doNothing() === undefined);//true
+
+// task
+// function min(a, b) {
+//   return a < b ? a : b;
+// }
+
+// console.log(min(2, -5));
+
+// function pow(x, n) {
+//   res = 1;
+
+//   for (let i = 0; i < n; ++i) {
+//     res = res * x;
+//   }
+//   return res;
+// }
+
+// console.log(pow(2, 5)); //32
+
+// 2.16 Function Expression
+// --> Theres is another syntax for creating a function that is called a Function Expression. It allows us to create a new function in the midddle of any expression.
+
+// --> Observe that there is no name after the function keyword. Omitting a name is allowed for function Expression.
+// let sayHello = function () {
+//   console.log("Hello");
+// };
+// console.log(sayHello());
+
+// --> Function is a value
+
+// function sayHi() { // The function Declaration creates the function and puts it into the variable named sayHi
+//   console.log("Hello");
+// }
+
+// let a = sayHi; // This line copies the variable into "a" , as a result the function can be called as both sayHi() and a().
+
+// a(); // Hello
+// sayHi(); // Hello
+
+// --> Function Expression have ";" at the end, but Function Decleration does not.
+
+// --> Revisit Function Expressions later
+
+// -->2.17 Arrow Functions, the basics
+// --> Arrow functions can be used as replacements of function expressions
+
+// --> Syntax: let func = (arg1, arg2, ..., argN) => expression;
+
+// let sum = (a, b) => a + b;
+// console.log(sum(2, 3)); // 5
+
+// let double = (n) => n * 2;
+// console.log(double(2)); // 4
+
+// let sayHi = () => console.log("hello");
+// sayHi(); // hello
+
+// let factorial = (a) => {
+//   let res = 1;
+//   for (let i = a; i > 1; --i) {
+//     res *= i;
+//   }
+//   return res;
+// };
+
+// console.log(factorial(5)); // 120
+
+// --> 3.1 Revisit Debugging in browser
+
+// --> Note: backtick quotes "`" allow to split the string into multiple lines
+
+// let str = `
+//   hello
+//   I
+//   am
+//   anurag
+//   `;
+// console.log(str);
---
+++ mocha.js
@@ -0,0 +1,15 @@
+// --> 3.5 Automated testing with Mocha
+// --> Behaviour Driven Development (BDD). BDD is three things in one: tests, documentation, and examples.
+
+// --> Before creating the function of pow, we can imagine what the function should do and describe it. Such description is called specification or spec.
+
+// describe("pow", function () {
+//   it("raises to n-th power", function () {
+//     assert.equal(pow(2, 3), 8);
+//   });
+// });
+
+// --> Link to check the development flow of automated testing
+// --> Mocha is the core framework that provides common testing functions including describe and it and the main functions that runs tests.
+// --> Chai is library with many assertions.
+// --> Sinon is the library to spy over functions, emulate build-in functions and more, we'll need it much later.
